16b057eba8ea9a614a12a61df04cdbe907eeff3c
acardenaslara <acardenaslara@iesesteveterradas.cat>
2018-05-10 20:29:09 +0200

    nueva plantilla 2

--- resources/views/layouts/menuSuperior.blade.php
+++ resources/views/layouts/menuSuperior.blade.php
@@ -15,21 +15,23 @@
 
   <!-- Entrar/Salir -->
   @guest
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle mr-lg-2" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <span>Acceder</span>
-      <div class="dropdown-menu dropdown-menu-right">
-        <a class="dropdown-item" href="#">
-          Login 
-        </a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">
-          Registrate
-        </a>
-      </div>
-    </a>
+  <li class="nav-item">
+      <a class="nav-link" href="{{ route('login') }}">Acceder</a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="{{ route('register') }}">Registrate</a>
   </li>
   @else
+  @if ( Auth::user()->rol === "admin" )
+  <li class="nav-item" id="admin">
+    <a class="nav-link" href="#">Panel de Administrador</a>
+  </li>
+  @elseif ( Auth::user()->rol === "desarrollador")
+  <li class="nav-item" id="develop">
+    <a class="nav-link" href="#">Panel de Desarollador</a>
+  </li>
+  @endif
   <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle mr-lg-2" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
@@ -37,13 +39,19 @@
 
     </a>
     <div class="dropdown-menu dropdown-menu-right">
-      <a class="dropdown-item" href="#">
+      <a class="dropdown-item" href="{{ route('perfil') }}">
         <i class="fas fa-user"></i> Mi Perfil 
       </a>
       <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">
+      <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    <i class="icon-key">
         <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
       </a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+      </form>
     </div>
   </li>
   @endguest
