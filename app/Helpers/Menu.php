cb25bab4eda71762b9abb60aa6ca38bd06e6d3c6
mcolominas <mcolominasrojas@iesesteveterradas.cat>
2018-05-10 20:23:03 +0200

    menu con categorias

--- app/Helpers/Menu.php
+++ app/Helpers/Menu.php
@@ -6,15 +6,12 @@ use App\Models\Categoria;
 class Menu{
 	
 	public static function getMenuIndex(){
-		$categorias = Categoria::select("nombre", "slug", "img")->orderby("nombre")->get();
-		
-		$categorias->each(function($model){
+		$categorias = Categoria::select("nombre", "slug", "img")->orderby("nombre")->get()->each(function($model){
 			$model->setUrl("indexCategoria");
-		});
-		$categorias = $categorias->toArray();
+		})->toArray();
 
-		array_unshift($categorias, ["nombre" => "Todos los juegos", "img" => "", "url" => route("index")]);
+		array_unshift($categorias, ["nombre" => "Todos los juegos", "img" => "#", "url" => route("index")]);
 
-		return(json_encode($categorias));
+		return($categorias);
 	}
 }
